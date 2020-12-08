012c202dd60f8b032bdb567766510498fc4364ee
libingchen <3952700@qq.com>
2020-12-08 15:53:59 +0800

    detail use route

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,20 +8,17 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
   heroes: Hero[];
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
     this.getHeroes();
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroes = heroes);
+    .subscribe(heroes => this.heroes = heroes);
   }
+  
 }
